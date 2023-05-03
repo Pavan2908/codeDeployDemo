d14b137a6f2a9f961d40c6ab805bbcb495bffd73
Pavan290803 <tatireddypavan29@gmail.com>
2023-05-04 00:29:49 +0530

    Initial Commit

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! I am deployed from github * 3')
+        'body': json.dumps('Hello from Lambda! I am deployed from github * 4')
     }
